63b2e56a91d99f881ebdb9eeec173e613fbd1ebf
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-06 14:25:11 +0530

    made some changes in fetched data parsing.

--- practice-1/src/App.jsx
+++ practice-1/src/App.jsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import CreatingData from "./component/CreatingData";
-
+import FetchingData from "./component/FetchingData";
 
 function App() {
-
   return (
     <>
       <CreatingData />
+      <FetchingData />
     </>
   );
 }
--- practice-1/src/component/FetchingData.jsx
+++ practice-1/src/component/FetchingData.jsx
@@ -1,28 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
+const url = "https://react-http-a309a-default-rtdb.firebaseio.com/user.json";
+
 const FetchingData = () => {
   const [fetchedData, setFetchedData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  // console.log(fetchedData);
+
   const fetchingData = async () => {
     setIsLoading(true);
-    const response = await axios(
-      "https://jsonplaceholder.typicode.com/users/1"
-    );
-    // console.log(response.data);
-    const resdata = response.data;
-    const extractedData = {
-      name: resdata.name,
-      email: resdata.email,
-      username: resdata.username,
-    };
-    setFetchedData(extractedData);
+    try {
+      const response = await axios(url);
+      // console.log("reponse data " + JSON.stringify(response.data));
+      // const resdata = JSON.stringify(response.data);
+      // const userObject = JSON.parse(resdata);
+      const userData = Object.values(response.data);
+      setFetchedData(userData);
+    } catch (error) {
+      throw new Error("somthing wrong!!!");
+    }
+
     setIsLoading(false);
   };
-
-//   console.log(fetchedData);
-
   useEffect(() => {
     fetchingData();
   }, []);
@@ -30,13 +31,19 @@ const FetchingData = () => {
   return (
     <div>
       {isLoading && <h3>Loading...</h3>}
-      {!isLoading && (
-        <>
-          <h2>{fetchedData.name}</h2>
-          <h2>{fetchedData.username}</h2>
-          <h2>{fetchedData.email}</h2>
-        </>
-      )}
+
+      {!isLoading &&
+        fetchedData.map((user) => {
+          return (
+            <div key={user.email}>
+              <ul>
+                <li>{user.fullname}</li>
+                <li>{user.username}</li>
+                <li>{user.email}</li>
+              </ul>
+            </div>
+          );
+        })}
     </div>
   );
 };
