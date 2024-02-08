12976d6d424666fec623ffc98cdc40549cb3c9b4
Bablu Mehta <babloomehta895012@gmail.com>
2024-02-08 10:46:25 +0530

    fixed some errors

--- practice-3/src/App.jsx
+++ practice-3/src/App.jsx
@@ -1,24 +1,22 @@
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Root from './pages/Root'
-import Home from './pages/Home'
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import Home from "./pages/Home";
 
 function App() {
-
-  const rounter = createBrowserRouter([
-    {path:'/',
-    element: <Root />,
-    children:[
-      {index:true, element:<Home />}
-    ]
-  }
-  ])
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      children: [{ index: true, element: <Home /> }],
+    },
+  ]);
 
   return (
     <>
-     <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
